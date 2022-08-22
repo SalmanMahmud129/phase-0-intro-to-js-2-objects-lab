1c594900fdd34f1b30d332b0658111989e427fae
Salman Mahmud <salman_mahmud@hotmail.com>
2022-08-21 22:24:23 -0400

    commit tree 24fad29181cc13d6be269d6ddabfb78ddcb64976

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution in this file!
+const employee = {name: "Salman", streetAddress: "test"}
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    const newObj = {...obj}
+
+    newObj[key] = value
+    
+
+    //console.log(newObj)
+    return newObj
+}
+
+
+//updateEmployeeWithKeyAndValue(employee, "hairColor", "Black")
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value
+
+    return obj
+}
+
+function deleteFromEmployeeByKey(obj, key){
+    const newObj = {...obj}
+
+    //delete newObj.key
+    delete newObj[key] 
+
+    return newObj
+}
+
+
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key]
+
+    return obj
+}
